4b255888f69aa011cc86814214816c29866dffe4
TiagoM13 <thyagomota2000@gmail.com>
2022-12-07 21:44:31 -0300

    fix: remove and of line

--- web/src/components/CloseButton.tsx
+++ web/src/components/CloseButton.tsx
@@ -4,7 +4,7 @@ import { X } from "phosphor-react";
 export function CloseButton() {
   return (
     <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Fechar bformulário de feedback">
-      <X weight="bold" className="w-4 h-4"/>
+      <X weight="bold" className="w-4 h-4" />
     </Popover.Button>
   )
 }
